e8f5b65d0f49907c2e57b197e7c6419b9569242a
Goshective <den,skvortsoff@gmail.com>
2024-11-15 00:42:06 +0300

    Add control of versions of models

--- use_model_practice.py
+++ use_model_practice.py
@@ -1,27 +1,37 @@
+from csv import reader
+from transformers import pipeline, AutoTokenizer, AutoModelForSeq2SeqLM
+
 """Import and Translation"""
 
+CUR_MODEL_VERSION = 1
+CUR_MODEL_NAME = ""
+
+with open('model_names.csv', 'r') as f:
+    csvreader = reader(f, delimiter=';')
+    next(csvreader, None)
+    for model_id, model_name in csvreader:
+        if int(model_id) == CUR_MODEL_VERSION:
+            CUR_MODEL_NAME = model_name
+            break
+
+if not CUR_MODEL_NAME:
+    exit(0)
 
 text_en_ru = "translate English to Russian: Legumes share resources with nitrogen-fixing bacteria."
 text_ru_en = "translate Russian to English: Бобовые растения делят ресурсы с азотфиксирующими бактериями."
 
-from transformers import pipeline
-
 # Change `xx` to the language of the input and `yy` to the language of the desired output.
 # Examples: "en" for English, "fr" for French, "de" for German, "es" for Spanish, "zh" for Chinese, etc; translation_en_to_fr translates English to French
 # You can view all the lists of languages here - https://huggingface.co/languages
-translator = pipeline("translation_ru_to_en", model="Goshective/my_awesome_opus_books_model")
+translator = pipeline("translation_ru_to_en", model=CUR_MODEL_NAME)
 translator(text_ru_en)
 
 
-from transformers import AutoTokenizer
-
-tokenizer = AutoTokenizer.from_pretrained("Goshective/my_awesome_opus_books_model")
+tokenizer = AutoTokenizer.from_pretrained(CUR_MODEL_NAME)
 inputs = tokenizer(text_ru_en, return_tensors="pt").input_ids
 
 
-from transformers import AutoModelForSeq2SeqLM
-
-model = AutoModelForSeq2SeqLM.from_pretrained("Goshective/my_awesome_opus_books_model")
+model = AutoModelForSeq2SeqLM.from_pretrained(CUR_MODEL_NAME)
 outputs = model.generate(inputs, max_new_tokens=40, do_sample=True, top_k=30, top_p=0.95)
 
 tokenizer.decode(outputs[0], skip_special_tokens=True)
